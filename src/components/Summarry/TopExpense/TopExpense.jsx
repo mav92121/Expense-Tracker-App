7e7e75c258d8b91c7badb26794bf0a4ca2a82c54
mohsinali <mohsinali530121@gmail.com>
2024-12-07 14:38:09 +0530

    top expense chart

--- src/components/Summarry/TopExpense/TopExpense.jsx
+++ src/components/Summarry/TopExpense/TopExpense.jsx
@@ -1,11 +1,43 @@
 import React from "react";
 import "./TopExpense.css";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const TopExpense = () => {
+  const data = [
+    { name: "Entertainment", value: 400 },
+    { name: "Food", value: 300 },
+    { name: "Travel", value: 100 },
+  ];
+
   return (
     <div>
       <h3 className="heading">Top Expense</h3>
-      <div className="expense-container"></div>
+      <div className="expense-container">
+        <ResponsiveContainer width="100%" height={200}>
+          <BarChart
+            layout="vertical"
+            data={data}
+            margin={{ top: 5, right: 20, left: 20, bottom: 5 }}
+          >
+            <XAxis type="number" hide />
+            <YAxis dataKey="name" type="category" width={100} />
+            <Tooltip />
+            <Bar
+              dataKey="value"
+              fill="#9b88ff"
+              radius={[0, 30, 30, 0]}
+              barSize={20}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
